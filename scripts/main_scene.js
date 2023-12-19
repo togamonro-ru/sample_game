d8c56386b01c757ce8899c01d4146d1a6bbf8b19
togamonro-ru <a.togasawa.sys22@morijyobi.ac.jp>
2023-12-19 14:17:07 +0900

    太郎と花子を配置した #5

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -10,15 +10,21 @@ class MainScene extends Phaser.Scene {
 
     // シーンの事前読み込み処理
     preload() {
-         // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('back', 'assets/back.png');
-    }
+        // 画像の読み込み(使用する時の名前, パス)
+       this.load.image('back', 'assets/back.png');
+       this.load.image('hanako', 'assets/hanako.png');
+       this.load.image('taro', 'assets/taro.png');
+   }
 
-    // シーン初期化処理
-    create() {
-         // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(400, 300, 'back');
-        }
+   // シーン初期化処理
+   create() {
+    // 単体画像をシーンに追加(X座標,Y座標,画像名)
+   this.add.image(400, 300, 'back');
+   const taro = this.physics.add.sprite(500, 350, 'taro')
+   const hanako = this.physics.add.sprite(500, 350, 'hanako')
+   this.taro = taro
+   this.hanako = hanako
+   }
 
      // 毎フレーム実行される繰り返し処理
     update(time, delta) {
